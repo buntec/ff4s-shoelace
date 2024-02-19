e985fbbd7355c230e37d3a7df245cc77467b6942
Christoph Bunte <christophbunte@gmail.com>
2024-02-19 16:48:32 +0100

    more examples; dark mode

--- examples/src/main/scala/examples/example1/App.scala
+++ examples/src/main/scala/examples/example1/App.scala
@@ -2,16 +2,24 @@ package examples.example1
 
 import cats.effect.Async
 import cats.syntax.all._
+import cats.effect.implicits._
+
+import fs2.Stream
 
 import scalajs.js
+import scala.concurrent.duration.*
+import org.scalajs.dom
 
 case class State(
+    darkMode: Boolean = false,
     rangeValue: Int = 25,
     checkboxChecked: Boolean = false,
     dropdownChecked: Boolean = false,
     selectValue: Option[String] = None,
     multiSelectValue: List[String] = Nil,
-    switchChecked: Boolean = false
+    switchChecked: Boolean = false,
+    inputValue: Option[String] = None,
+    progress: Int = 0
 )
 
 sealed trait Action
@@ -32,12 +40,51 @@ class App[F[_]](implicit val F: Async[F]) extends ff4s.App[F, State, Action] {
       }
     )
 
+    _ <- Stream
+      .fixedDelay(100.millis)
+      .zipWithIndex
+      .map(_._2)
+      .evalMap(i =>
+        store.dispatch(Action.ModifyState(_.copy(progress = i.toInt % 100)))
+      )
+      .compile
+      .drain
+      .background
+
+    _ <- store.state
+      .map(_.darkMode)
+      .discrete
+      .changes
+      .evalMap(darkMode =>
+        if (darkMode) {
+          F.delay(dom.document.body.className = "dark sl-theme-dark")
+        } else {
+          F.delay(dom.document.body.className = "")
+        }
+      )
+      .compile
+      .drain
+      .background
+
   } yield store
 
   import dsl._
   import dsl.html._
 
   import ff4s.{shoelace => sl}
+
+  val darkMode = useState(state =>
+    div(
+      cls := "absolute top-0 right-0 py-2 px-4",
+      sl.Switch(
+        sl.Switch.checked := state.darkMode,
+        sl.Switch.onChange := (ev =>
+          Action.ModifyState(_.copy(darkMode = ev.target.checked)).some
+        ),
+        "Dark mode"
+      )
+    )
+  )
 
   val alert = sl.Alert(
     sl.Alert.open := true,
@@ -86,6 +133,25 @@ class App[F[_]](implicit val F: Async[F]) extends ff4s.App[F, State, Action] {
     sl.Badge(sl.Badge.variant := "danger", "Danger")
   )
 
+  val button = div(
+    cls := "flex flex-row items-center gap-2",
+    sl.Button(sl.Button.variant := "default", "Default"),
+    sl.Button(sl.Button.variant := "primary", "Primary"),
+    sl.Button(sl.Button.variant := "success", "Success"),
+    sl.Button(sl.Button.variant := "neutral", "Neutral"),
+    sl.Button(sl.Button.variant := "warning", "Warning"),
+    sl.Button(sl.Button.variant := "danger", "Danger")
+  )
+
+  val buttonGroup = div(
+    sl.ButtonGroup(
+      sl.ButtonGroup.label := "Alignment",
+      sl.Button("Left"),
+      sl.Button("Center"),
+      sl.Button("Right")
+    )
+  )
+
   val checkbox = useState(state =>
     sl.Checkbox(
       sl.Checkbox.checked := state.checkboxChecked,
@@ -130,6 +196,43 @@ class App[F[_]](implicit val F: Async[F]) extends ff4s.App[F, State, Action] {
           else
             None
         }
+      )
+    )
+  )
+
+  val input = useState(state =>
+    div(
+      s"Hello, ${state.inputValue.getOrElse("...")}",
+      sl.Input(
+        sl.Input.label := "What is your name?",
+        sl.Input.clearable := true,
+        sl.Input.value := state.inputValue.getOrElse(""),
+        sl.Input.onInput := (ev =>
+          Action
+            .ModifyState(
+              _.copy(inputValue = ev.target.value.some.filter(_.nonEmpty))
+            )
+            .some
+        )
+      )
+    )
+  )
+
+  val progressBar = useState(state =>
+    div(
+      cls := "w-64",
+      sl.ProgressBar(
+        sl.ProgressBar.value := state.progress
+      )
+    )
+  )
+
+  val progressRing = useState(state =>
+    div(
+      cls := "flex flex-row items-center justify-center",
+      sl.ProgressRing(
+        styleAttr := "--size: 32px;",
+        sl.ProgressRing.value := state.progress
       )
     )
   )
@@ -204,6 +307,7 @@ class App[F[_]](implicit val F: Async[F]) extends ff4s.App[F, State, Action] {
   )
 
   val tag = div(
+    cls := "flex flex-row items-center gap-2",
     sl.Tag(sl.Tag.variant := "primary", "Primary"),
     sl.Tag(sl.Tag.variant := "success", "Success"),
     sl.Tag(sl.Tag.variant := "neutral", "Neutral"),
@@ -216,34 +320,44 @@ class App[F[_]](implicit val F: Async[F]) extends ff4s.App[F, State, Action] {
     sl.Button("hover me")
   )
 
-  override val view =
-    div(
-      idAttr := "page",
-      span(cls := "heading", "Shoelace components"),
-      span(cls := "component-heading", "Alert"),
-      alert,
-      span(cls := "component-heading", "Animation"),
-      animation,
-      span(cls := "component-heading", "Avatar"),
-      avatar,
-      span(cls := "component-heading", "Badge"),
-      badge,
-      span(cls := "component-heading", "Checkbox"),
-      checkbox,
-      span(cls := "component-heading", "Copy Button"),
-      copyButton,
-      span(cls := "component-heading", "Dropdown"),
-      dropDown,
-      span(cls := "component-heading", "Range"),
-      range,
-      span(cls := "component-heading", "Select"),
-      select,
-      span(cls := "component-heading", "Switch"),
-      switch,
-      span(cls := "component-heading", "Tag"),
-      tag,
-      span(cls := "component-heading", "Tooltip"),
-      tooltip
-    )
+  override val view = div(
+    idAttr := "page",
+    darkMode,
+    span(cls := "heading", "Shoelace components"),
+    span(cls := "component-heading", "Alert"),
+    alert,
+    span(cls := "component-heading", "Animation"),
+    animation,
+    span(cls := "component-heading", "Avatar"),
+    avatar,
+    span(cls := "component-heading", "Badge"),
+    badge,
+    span(cls := "component-heading", "Button"),
+    button,
+    span(cls := "component-heading", "Button Group"),
+    buttonGroup,
+    span(cls := "component-heading", "Checkbox"),
+    checkbox,
+    span(cls := "component-heading", "Copy Button"),
+    copyButton,
+    span(cls := "component-heading", "Dropdown"),
+    dropDown,
+    span(cls := "component-heading", "Input"),
+    input,
+    span(cls := "component-heading", "Progress Bar"),
+    progressBar,
+    span(cls := "component-heading", "Progress Ring"),
+    progressRing,
+    span(cls := "component-heading", "Range"),
+    range,
+    span(cls := "component-heading", "Select"),
+    select,
+    span(cls := "component-heading", "Switch"),
+    switch,
+    span(cls := "component-heading", "Tag"),
+    tag,
+    span(cls := "component-heading", "Tooltip"),
+    tooltip
+  )
 
 }
